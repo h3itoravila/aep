25294bcf68aaee9d4f19e5d10fc1f160602ce013
Heitor Silva Avila <87899649+h3itoravila@users.noreply.github.com>
2021-10-02 22:15:26 -0300

    Add files via upload

---
+++ matrizes-vetores/Exercício 16.2.c
@@ -0,0 +1,66 @@
+/* 16.2 Ler 2 matrizes, A 4x6 e B 4x6 e criar:
+a) Uma matriz S que seja a soma de A e B.
+b) Uma matriz D que seja a diferença de A e B.
+Escrever as matrizes S e D após todo cálculo estar concluído. */
+
+// Aluno: Heitor Silva Avila
+
+#include <stdio.h>	// Inicializando o I/O
+
+int main () { 
+	int ma[4][6], mb[4][6], ms[4][6], md[4][6], a = 0, b = 0, c = 0;
+    printf("Obtendo matriz A: \n");
+	// Estrutura de repetição para leitura de dados
+	for (b = 0; b < 4; b++) {
+		for (a = 0; a < 6; a++) {
+			printf("Linha %d, ", b+1);
+			printf("coluna %d: ", a+1);
+			scanf("%d", &ma[b][a]);	
+		}
+		printf("\n");
+	}
+	printf("\n");
+    printf("Obtendo matriz B: \n");
+	// Estrutura de repetição para leitura de dados
+	for (b = 0; b < 4; b++) {
+		for (a = 0; a < 6; a++) {
+			printf("Linha %d, ", b+1);
+			printf("coluna %d: ", a+1);
+			scanf("%d",&mb[b][a]);	
+		}
+		printf("\n");
+	}
+	printf("\n");
+    // Criando a matriz S com a soma de A e B
+	for (b = 0; b < 4; b++){
+		for (a = 0; a < 6; a++) { 
+        	c = ma[b][a] + mb[b][a];	
+            ms[b][a] = c;
+            c = 0;
+		}
+	}
+    // Criando a matriz D com a diferença de A e B
+	for (b = 0; b < 4; b++){
+		for (a = 0; a < 6; a++) {
+        	c = ma[b][a] - mb[b][a];	
+            md[b][a] = c;
+            c = 0;
+		}
+	}
+	printf("Imprimindo a matriz S (A + B): \n \n");
+	for (b = 0; b < 4; b++){
+		for (a = 0; a < 6; a++) {
+			printf("%d ", ms[b][a]);	
+		}
+		printf("\n"); 
+	}
+	printf("\n");
+	printf("Imprimindo a matriz D (A - B): \n \n");
+	for (b = 0; b < 4; b++){
+		for (a = 0; a < 6; a++) {
+			printf("%d ", md[b][a]);	
+		}
+		printf("\n"); 
+	}
+	printf("\n");
+}
---
+++ matrizes-vetores/Exercício 16.3.c
@@ -0,0 +1,61 @@
+/* 16.3.Ler uma matriz A de 4 x 4, calcular e escrever as somas dos elementos marcados com o X.
+Utilizar estruturas de repetição. 
+XX-- | ---- | X--- | -XXX
+XX-- | ---- | XX-- | --XX
+---- | --XX | XXX- | ---X
+---- | --XX | XXXX | ----   */
+
+// Aluno: Heitor Silva Avila
+
+#include <stdio.h> // Inicializando I/O
+
+int main () {
+	int ma[4][4], a = 0, b = 0, c = 0, d = 0;
+	// Estrutura de repetição para leitura de dados
+	for (b = 0; b < 4; b++){
+		for (a = 0; a < 4; a++) {
+			printf("Linha %d, ", b+1);
+			printf("coluna %d: ", a+1);
+			scanf("%d",&ma[b][a]);	
+		}
+		printf("\n");
+	}
+    // Calculando a primeira soma
+    printf("Primeira soma: ");
+    for (b = 0; b < 2; b++) {
+        for (a = 0; a < 2; a++) {
+            c = c + ma[b][a];
+        }
+    }
+    printf("%d \n", c);
+    c = 0;
+    // Calculando a segunda soma
+    printf("Segunda soma: ");
+    for (b = 2; b < 4; b++) {
+        for (a = 2; a < 4; a++) {
+            c = c + ma[b][a];
+        }
+    }
+    printf("%d \n", c);
+    c = 0;
+    // Calculando a terceira soma
+    printf("Terceira soma: ");
+    for (b = 0; b < 4; b++) {
+        for (a = 0; a < b+1; a++) {
+            c = c + ma[b][a];
+        }
+    }
+    printf("%d \n", c);
+    c = 0;
+    // Calculando a quarta soma
+    printf("Quarta soma: ");
+    for (b = 0; b < 4; b++) {
+        a = b + 1;
+        while (a != 4) {
+            c = c + ma[b][a];
+            a++;
+        }
+    }
+    printf("%d \n", c);
+    c = 0;
+}
