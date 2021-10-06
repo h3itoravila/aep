b07c7b907c936f8eeaf95c11cc6a7a67c7784fef
Heitor Silva Avila <87899649+h3itoravila@users.noreply.github.com>
2021-10-06 06:22:36 -0300

    Update somatorio.c

--- preencher-vetor/somatorio.c
+++ preencher-vetor/somatorio.c
@@ -1,17 +1,12 @@
-#include <stdio.h>	// Inicializando o I/O
-#include <stdlib.h>	// Para uso do systempause
-#include <locale.h>	// Corrigir acentuação
+#include <stdio.h>
 
-int main () { 
-	setlocale(LC_ALL,"Portuguese");	// Corrigir acentuação
-	int vetor[9], a = 0, b = 0;		// Vetor de 10 posições
-	for (a = 0; a < 10; a++) {		// Repetição para obter valores
-		vetor[a]=(a+1)*10;				// Valores definidos no código
+int main (void) { 
+	int vetor[9], a = 0, b = 0;
+	for (a = 0; a < 10; a++) {
+		vetor[a]=(a+1)*10;
 	}
 	printf("Vetor: ");
-	for (a = 0; a < 10; a++) {		// Variável auxiliar
-		printf("%d ", vetor[a]);	// Saída de dados
+	for (a = 0; a < 10; a++) {
+		printf("%d ", vetor[a]);
 	}
-	printf("\n");					// Quebra de linha estética
-	system("PAUSE");				// Evita encerramento acidental
 }
