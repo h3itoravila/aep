784e87171f3eb384ca734b99cc5e7e04ca876406
Heitor Silva Avila <87899649+h3itoravila@users.noreply.github.com>
2021-10-06 05:27:40 -0300

    Create preenchervetor.c

---
+++ preencher-vetor/preenchervetor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>	// Inicializando o I/O
+#include <stdlib.h>	// Para uso do systempause
+#include <locale.h>	// Corrigir acentuação
+
+int main () { 
+	setlocale(LC_ALL,"Portuguese");	// Corrigir acentuação
+	int vetor[9], a = 0;			// Vetor de 10 posições
+	for (a = 0; a < 10; a++) {		// Repetição para obter valores
+		vetor[a]=30;				// Valores definidos no código
+	}
+	printf("Vetor: ");
+	for (a = 0; a < 10; a++) {		// Variável auxiliar
+		printf("%d ", vetor[a]);	// Saída de dados
+	}
+	printf("\n");					// Quebra de linha estética
+	system("PAUSE");				// Evita encerramento acidental
+}
